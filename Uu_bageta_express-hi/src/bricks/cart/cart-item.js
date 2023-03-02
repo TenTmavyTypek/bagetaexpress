bdd8101ad8dfe24a1347ab8945e55ba014632d9f
Tomas Zifcak <tomas.zifcak197@gmail.com>
2023-03-02 14:51:02 +0100

    visual changes to item lists

--- Uu_bageta_express-hi/src/bricks/cart/cart-item.js
+++ Uu_bageta_express-hi/src/bricks/cart/cart-item.js
@@ -1,6 +1,5 @@
 //@@viewOn:imports
 import { createVisualComponent, Utils, useCall, useEffect, useState } from "uu5g05";
-import Uu5TilesElements from "uu5tilesg02-elements";
 import Uu5Elements from "uu5g05-elements";
 import Uu5Imaging from "uu5imagingg01";
 import Config from "./config/config.js";
@@ -14,7 +13,7 @@ const content = { category: "interface", segment: "content" };
 
 //@@viewOn:css
 const Css = {
-  main: () => Config.Css.css({}),
+  main: () => Config.Css.css({ borderBottom: "1px solid #ccc", paddingTop: "1rem" }),
 };
 //@@viewOff:css
 
@@ -58,67 +57,63 @@ const CartItem = createVisualComponent({
 
     return currentNestingLevel && state == "ready" && data !== undefined ? (
       <div {...attrs}>
-        <Uu5TilesElements.Tile>
-          {"\xA0"}
-          <Uu5Elements.Grid
-            flow="column"
-            templateColumns={{ xs: "100%", m: "0.2fr 1fr 1fr 0.2fr" }}
-            templateAreas={{
-              xs: `img, heading, content, price, count`,
-              m: `
+        <Uu5Elements.Grid
+          flow="column"
+          templateColumns={{ xs: "100%", m: "0.2fr 1fr 1fr 0.2fr" }}
+          templateAreas={{
+            xs: `img, heading, content, price, count`,
+            m: `
           count img heading price,
           count img content price`,
-            }}
-            rowGap={{ xs: "2rem", m: "1rem" }}
-          >
-            <Uu5Elements.Grid.Item alignSelf="center" gridArea="img">
-              <Uu5Imaging.Image src={data.image} shape="rect2x1" />
-            </Uu5Elements.Grid.Item>
-            <Uu5Elements.Grid.Item gridArea="heading" justifySelf="center" alignSelf="center">
-              <Uu5Elements.Text category="expose" segment="default" type="lead">
-                {data.name}
+          }}
+          rowGap={{ xs: "2rem", m: "1rem" }}
+        >
+          <Uu5Elements.Grid.Item alignSelf="center" gridArea="img">
+            <Uu5Imaging.Image src={data.image} shape="rect2x1" />
+          </Uu5Elements.Grid.Item>
+          <Uu5Elements.Grid.Item gridArea="heading" justifySelf="center" alignSelf="center">
+            <Uu5Elements.Text category="expose" segment="default" type="lead">
+              {data.name}
+            </Uu5Elements.Text>
+          </Uu5Elements.Grid.Item>
+          <Uu5Elements.Grid.Item gridArea="content" alignSelf="center">
+            <Uu5Elements.Grid rowGap="0.1rem" justifyContent="center" justifyItems="center">
+              <Uu5Elements.Text {...title} type="micro">
+                Hmotnosť:
+                <Uu5Elements.Text {...content} type="large">
+                  {" "}
+                  {data.weight + "g"}
+                </Uu5Elements.Text>
               </Uu5Elements.Text>
-            </Uu5Elements.Grid.Item>
-            <Uu5Elements.Grid.Item gridArea="content" alignSelf="center">
-              <Uu5Elements.Grid rowGap="0.1rem" justifyContent="center" justifyItems="center">
-                <Uu5Elements.Text {...title} type="micro">
-                  Hmotnosť:
-                  <Uu5Elements.Text {...content} type="large">
-                    {" "}
-                    {data.weight + "g"}
-                  </Uu5Elements.Text>
+              <Uu5Elements.Text {...title} type="micro">
+                Ingrediencie:
+                <Uu5Elements.Text {...content} type="large">
+                  {" "}
+                  {data.ingredients + " "}
                 </Uu5Elements.Text>
-                <Uu5Elements.Text {...title} type="micro">
-                  Ingrediencie:
-                  <Uu5Elements.Text {...content} type="large">
-                    {" "}
-                    {data.ingredients + " "}
-                  </Uu5Elements.Text>
-                </Uu5Elements.Text>
-                <Uu5Elements.Text {...title} type="micro">
-                  Alergény:
-                  <Uu5Elements.Text {...content} type="large">
-                    {" "}
-                    {data.allergens + " "}
-                  </Uu5Elements.Text>
-                </Uu5Elements.Text>
-              </Uu5Elements.Grid>
-            </Uu5Elements.Grid.Item>
-            <Uu5Elements.Grid.Item gridArea="price" justifySelf="center" alignSelf="center">
-              <Uu5Elements.Grid justifyItems="center">
-                <Uu5Elements.Text category="expose" segment="default" type="lead">
-                  {data.price * props.data.numberOrdered}€{"\xA0"}
-                </Uu5Elements.Text>
-              </Uu5Elements.Grid>
-            </Uu5Elements.Grid.Item>
-            <Uu5Elements.Grid.Item gridArea="count" justifySelf="center" alignSelf="center">
-              <Uu5Elements.Text category="expose" segment="default" type="lead">
-                {props.data.numberOrdered}x
               </Uu5Elements.Text>
-            </Uu5Elements.Grid.Item>
-          </Uu5Elements.Grid>
-          {"\xA0"}
-        </Uu5TilesElements.Tile>
+              <Uu5Elements.Text {...title} type="micro">
+                Alergény:
+                <Uu5Elements.Text {...content} type="large">
+                  {" "}
+                  {data.allergens + " "}
+                </Uu5Elements.Text>
+              </Uu5Elements.Text>
+            </Uu5Elements.Grid>
+          </Uu5Elements.Grid.Item>
+          <Uu5Elements.Grid.Item gridArea="price" justifySelf="center" alignSelf="center">
+            <Uu5Elements.Grid justifyItems="center">
+              <Uu5Elements.Text category="expose" segment="default" type="lead">
+                {data.price * props.data.numberOrdered}€{"\xA0"}
+              </Uu5Elements.Text>
+            </Uu5Elements.Grid>
+          </Uu5Elements.Grid.Item>
+          <Uu5Elements.Grid.Item gridArea="count" justifySelf="center" alignSelf="center">
+            <Uu5Elements.Text category="expose" segment="default" type="lead">
+              {props.data.numberOrdered}x
+            </Uu5Elements.Text>
+          </Uu5Elements.Grid.Item>
+        </Uu5Elements.Grid>
       </div>
     ) : null;
     //@@viewOff:render
--- Uu_bageta_express-hi/src/bricks/scan/scan-view.js
+++ Uu_bageta_express-hi/src/bricks/scan/scan-view.js
@@ -3,8 +3,6 @@ import { QrReader } from "react-qr-reader";
 import { createVisualComponent, Utils, useState, useCall } from "uu5g05";
 import Uu5Elements from "uu5g05-elements";
 import Uu5Forms from "uu5g05-forms";
-import Plus4U5 from "uu_plus4u5g02";
-import Plus4U5Elements from "uu_plus4u5g02-elements";
 import Config from "./config/config.js";
 import RouteBar from "../../core/route-bar.js";
 import Calls from "../../calls.js";
@@ -105,47 +103,46 @@ const ScanView = createVisualComponent({
     return currentNestingLevel ? (
       <div {...attrs}>
         <RouteBar />
-        <Plus4U5Elements.IdentificationBlock>
-          <Uu5Elements.Grid justifyContent="center">
-            {state === "error" ||
-              (state === "errorNoData" ? (
-                <Uu5Elements.Text {...title} type="major" colorScheme="negative">
-                  Neplatný kód
-                </Uu5Elements.Text>
-              ) : (
-                <Uu5Elements.Text {...title} type="major">
-                  Pin kód:
-                </Uu5Elements.Text>
-              ))}
-            <TextInput
-              value={manualPin}
-              pattern="[0-9][0-9][0-9][0-9]"
-              validateOnChange
-              onChange={(x) => (manualPin = x.data.value)}
-            />
-            <Uu5Elements.Button
-              size="xl"
-              onClick={() => call({ pin: manualPin }).then((data) => setData(data))}
-              colorScheme="yellow"
-              significance="highlighted"
-            >
-              {" "}
-              <Uu5Elements.Text colorScheme="building" {...title} type="micro">
-                <Uu5Elements.Icon icon="mdi-check" />
-                {"\xA0"}
-                Povrdiť
+        <Uu5Elements.Grid justifyContent="center">
+          {state === "error" ||
+            (state === "errorNoData" ? (
+              <Uu5Elements.Text {...title} type="major" colorScheme="negative">
+                Neplatný kód
               </Uu5Elements.Text>
-            </Uu5Elements.Button>
-            <QrReader
-              onResult={(result, error) => {
-                if (result) {
-                  handleCall(result?.text);
-                }
-              }}
-              style={{ width: "100%" }}
-            />
-          </Uu5Elements.Grid>
-        </Plus4U5Elements.IdentificationBlock>
+            ) : (
+              <Uu5Elements.Text {...title} type="major">
+                Pin kód:
+              </Uu5Elements.Text>
+            ))}
+          <TextInput
+            value={manualPin}
+            pattern="[0-9][0-9][0-9][0-9]"
+            validateOnChange
+            onChange={(x) => (manualPin = x.data.value)}
+          />
+          <Uu5Elements.Button
+            size="xl"
+            onClick={() => call({ pin: manualPin }).then((data) => setData(data))}
+            colorScheme="yellow"
+            significance="highlighted"
+          >
+            {" "}
+            <Uu5Elements.Text colorScheme="building" {...title} type="micro">
+              <Uu5Elements.Icon icon="mdi-check" />
+              {"\xA0"}
+              Povrdiť
+            </Uu5Elements.Text>
+          </Uu5Elements.Button>
+        </Uu5Elements.Grid>
+        <div style={{ maxWidth: "40rem", width: "100%", margin: "auto" }}>
+          <QrReader
+            onResult={(result, error) => {
+              if (result) {
+                handleCall(result?.text);
+              }
+            }}
+          />
+        </div>
       </div>
     ) : null;
     //@@viewOff:render
